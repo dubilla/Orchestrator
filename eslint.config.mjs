dd419f5f11bd9fe2c62ad598b722977db89fdf8f
dubilla <dan.ubilla@gmail.com>
2025-12-21 16:05:04 -0500

    Add project dependencies and configuration for testing and linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,9 +9,11 @@ const eslintConfig = defineConfig([
   globalIgnores([
     // Default ignores of eslint-config-next:
     ".next/**",
+    "orchestra-app/.next/**",
     "out/**",
     "build/**",
     "next-env.d.ts",
+    "jest.config.cjs",
   ]),
 ]);
 
